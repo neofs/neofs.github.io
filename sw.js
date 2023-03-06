f224459f6a2af8cca34eaf1ee0d53454fb844ee5
zk524 <20262010007@fudan.edu.cn>
2023-03-06 16:23:31 +0800

    dev

---
+++ sw.js
@@ -0,0 +1,5 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
+workbox.routing.registerRoute('/demo-my-pwa/index.html', workbox.strategies.networkFirst());
+workbox.routing.registerRoute(new RegExp('.*\.js'), workbox.strategies.networkFirst());
+workbox.routing.registerRoute(new RegExp('.*\.css'), workbox.strategies.networkFirst());
+workbox.routing.registerRoute(new RegExp('.*\.png'), workbox.strategies.networkFirst());
