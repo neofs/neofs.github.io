2cb1676b59ea409ec22feaf178aa7619b7c03d69
zk524 <20262010007@fudan.edu.cn>
2023-03-06 16:32:04 +0800

    dev

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
-workbox.routing.registerRoute('index.html', workbox.strategies.networkFirst());
+workbox.routing.registerRoute('/burger-admin/index.html', workbox.strategies.networkFirst());
