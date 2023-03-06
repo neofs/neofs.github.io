e2831c9ee2279811a75cb6c180d689455480e790
zk524 <20262010007@fudan.edu.cn>
2023-03-06 17:08:33 +0800

    dev

--- sw.js
+++ sw.js
@@ -1,6 +1,3 @@
-// importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js');
-// workbox.routing.registerRoute('/burger-admin/index.html', new workbox.strategies.NetworkFirst());
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.0.0/workbox-sw.js');
 workbox.routing.registerRoute('/burger-admin/index.html', workbox.strategies.networkFirst());
 workbox.routing.registerRoute(new RegExp('.*\.js'), workbox.strategies.networkFirst());
+workbox.routing.registerRoute(new RegExp('.*\.png'), workbox.strategies.networkFirst());
