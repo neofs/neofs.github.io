5ffcbc9c825482db01f4f2506ab42efe5796a6e1
zk524 <20262010007@fudan.edu.cn>
2023-03-08 11:15:50 +0800

    dev

--- src.js
+++ src.js
@@ -1,4 +1,13 @@
 _
+    ._([document.createElement('link')])._(([_]) => { _._({ rel: 'manifest' }) })._(([_]) => document.head.replaceChildren(_))._(([_]) => {
+        _.setAttribute("href", 'data:application/json;base64,' + btoa(JSON.stringify({
+            "name": "QR - ENC",
+            "icons": [{ "src": location.origin + "/favicon.png", "sizes": "192x192" }],
+            "start_url": location.origin,
+            "share_target": { "action": location.origin + "/share", "method": "POST", "enctype": "multipart/form-data", "params": { "title": "title", "text": "text", "url": "url", "files": [{ "name": "files", "accept": ["*/*", ".*"] }] } },
+            "display": "standalone"
+        })))
+    })
     ._([document.createElement('script')])
     ._([`https://cdnjs.cloudflare.com/ajax/libs/qrcode/1.5.1/qrcode.min.js`])
     ._([_ => {
@@ -18,25 +27,13 @@ _
     ._(([onload, src, _]) => [_._({ src, onload })])
     ._([document.createElement('meta')])
     ._(([_]) => { _._({ name: 'viewport', content: 'width=device-width, initial-scale=1.0' }) })
-    ._([document.createElement('link')])._(([_]) => {
-        const path = location.origin
-        _._({
-            rel: 'manifest', href: 'data:application/json;base64,' + btoa(JSON.stringify({
-                "name": "QR - ENC",
-                "icons": [{ "src": path + "/favicon.png", "sizes": "192x192" }],
-                "start_url": path,
-                "share_target": { "action": path + "/share", "method": "POST", "enctype": "multipart/form-data", "params": { "title": "title", "text": "text", "url": "url", "files": [{ "name": "files", "accept": ["*/*", ".*"] }] } },
-                "display": "standalone"
-            }))
-        })
-    })
     ._([document.createElement('link')])
     ._(([_]) => { _._({ rel: 'icon', href: 'favicon.png' }) })
     ._([document.createElement('meta')])
     ._(([_]) => { _.setAttribute('charset', 'UTF-8') })
     ._([document.createElement('title')])
     ._(([_]) => { _._({ innerText: 'QR - ENC' }) })
-    ._(_ => document.head.replaceChildren(..._))
+    ._(_ => document.head.append(..._))
     ._(_ => document.body.replaceChildren('LOADING ...'))
     ._(_ => document.body.style._({ textAlign: 'center' }))
     ._(_ => navigator.serviceWorker?.register('_.js'))
